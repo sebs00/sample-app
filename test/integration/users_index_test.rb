c1998eaa55037124616af731c143416d4a36bcff
sebs00 <73179860+sebs00@users.noreply.github.com>
2022-04-10 12:07:00 -0400

    Finish user edit, update, index and destroy actions

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -30,7 +30,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
       assert_select 'a[href=?]', user_path(user), text: 'delete' unless user == @admin
     end
 
-    assert_difference User.count, -1 do
+    assert_difference 'User.count', -1 do
       delete user_path(@non_admin)
     end
   end
